9e1ef0e848a36de01192e324663731994e498cd8
Nikita Filonov <nf@alemira.com>
2021-01-09 18:07:02 +0300

    lesson_4

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,11 @@
+import os
 import sys
+import pathlib
+from reports.report import ReportManager
+
+
+def absolute_dir():
+    return str(pathlib.Path(__file__).parent.absolute())[:-6]
 
 
 def parse(**kwargs):
@@ -8,3 +15,12 @@ def parse(**kwargs):
         if kwargs.get('default'):
             return kwargs.get('default')
         return False
+
+
+def clear_reports(*args):
+    for test_type in args:
+        if len(os.listdir(''.join([absolute_dir(), '/reports/{0}/reports'.format(test_type)]))) > 10:
+            ReportManager('/{0}/reports/'.format(test_type)).delete()
+
+
+clear_reports('ui', 'api')
